9e5b8bac89cf19b41269186eefe57d2bcad49eb7
robot269006 <robot269006@yahoo.co.jp>
2017-10-22 23:37:58 +0900

    Books controller and view
    rails g controller books index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'books/index'
+
   root to: 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
